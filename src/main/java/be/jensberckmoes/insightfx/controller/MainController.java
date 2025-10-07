22fcf34bebc3b54e65695ddaa8ac7de606b51e09
CrusaderSquirrel <jensberckmoes@hotmail.com>
2025-10-07 10:40:49 +0200

    Added functionality to the chart generation. Flow works as intended.

--- src/main/java/be/jensberckmoes/insightfx/controller/MainController.java
+++ src/main/java/be/jensberckmoes/insightfx/controller/MainController.java
@@ -5,29 +5,39 @@ import be.jensberckmoes.insightfx.model.DataRecord;
 import be.jensberckmoes.insightfx.service.AnalysisService;
 import be.jensberckmoes.insightfx.service.CsvParserService;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class MainController {
-
     @FXML
-    public TabPane tabPane;
+    private TabPane tabPane;
     @FXML
-    public TableView<CategorySummary> analysisTable;
+    private Tab analysisTab;
     @FXML
-    public TableColumn<CategorySummary, String> categoryColumn;
+    private Tab chartTab;
     @FXML
-    public TableColumn<CategorySummary, Integer> countColumn;
+    public Tab dataTab;
     @FXML
-    public TableColumn<CategorySummary, BigDecimal> totalColumn;
+    private TableView<CategorySummary> analysisTable;
+    @FXML
+    private TableColumn<CategorySummary, String> categoryColumn;
+    @FXML
+    private TableColumn<CategorySummary, Integer> countColumn;
+    @FXML
+    private TableColumn<CategorySummary, BigDecimal> totalColumn;
     @FXML
     private TableView<DataRecord> tableView;
     @FXML
@@ -41,12 +51,18 @@ public class MainController {
     @FXML
     private Label statusLabel;
     @FXML
-    private Button analyzeButton, chartButton, exportButton, loadButton;
+    private StackPane chartPane;
     @FXML
-    private Tab analysisTab;
+    private Button analyzeButton, chartButton, exportButton;
+    @FXML
+    public RadioButton csvRadioButton;
+    @FXML
+    public RadioButton pdfRadioButton;
 
     private final CsvParserService csvParserService = new CsvParserService();
     private final AnalysisService analysisService = new AnalysisService();
+    private List<CategorySummary> results = new ArrayList<>();
+    private List<DataRecord> records = new ArrayList<>();
 
     @FXML
     public void initialize() {
@@ -71,10 +87,14 @@ public class MainController {
         totalColumn.setCellValueFactory(cellData -> new javafx.beans.property.SimpleObjectProperty<>(
                 cellData.getValue().getTotal()
         ));
+        final ToggleGroup group = new ToggleGroup();
+        csvRadioButton.setToggleGroup(group);
+        pdfRadioButton.setToggleGroup(group);
     }
 
     @FXML
     private void onLoadCsv() {
+        resetForNewCSV();
         final FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select a CSV-file");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
@@ -83,13 +103,11 @@ public class MainController {
         if (Objects.isNull(file)) return;
 
         try (final InputStream stream = new FileInputStream(file)) {
-            final List<DataRecord> records = csvParserService.parse(stream);
+            records = csvParserService.parse(stream);
             tableView.setItems(FXCollections.observableArrayList(records));
 
             final boolean hasData = !records.isEmpty();
             analyzeButton.setDisable(!hasData);
-            chartButton.setDisable(!hasData);
-            exportButton.setDisable(!hasData);
             statusLabel.setText("CSV loaded: " + records.size() + " records");
         } catch (final Exception e) {
             new Alert(Alert.AlertType.ERROR, "Error loading CSV:\n" + e.getMessage(), ButtonType.OK).showAndWait();
@@ -105,22 +123,79 @@ public class MainController {
             return;
         }
 
-        final List<CategorySummary> results = analysisService.analyse(records);
+        results = analysisService.analyse(records);
         analysisTable.setItems(FXCollections.observableArrayList(results));
 
         analysisTab.setDisable(false);
         tabPane.getSelectionModel().select(analysisTab);
+        final boolean hasData = !results.isEmpty();
+        chartButton.setDisable(!hasData);
 
         statusLabel.setText("Analysis completed: " + results.size() + " categories");
     }
 
     @FXML
     private void onChart() {
-        statusLabel.setText("Generate Graphics... (to implement)");
+        if (results.isEmpty()) return;
+
+        final List<CategorySummary> categoryResults = results.stream()
+                .filter(r -> !r.getCategory().equals("Total Income") &&
+                        !r.getCategory().equals("Total Expenses") &&
+                        !r.getCategory().equals("Balance"))
+                .toList();
+
+        final double totalAmount = categoryResults.stream()
+                .mapToDouble(CategorySummary::getCount)
+                .sum();
+
+        final ObservableList<PieChart.Data> pieData = FXCollections.observableArrayList(
+                categoryResults.stream()
+                        .map(r -> new PieChart.Data(getNameWithPercent(r,totalAmount), r.getCount()))
+                        .toList()
+        );
+
+        final PieChart chart = new PieChart(pieData);
+        chart.setTitle("Expenses");
+        chart.setLabelLineLength(10);
+        chart.setLegendSide(Side.LEFT);
+
+        chartPane.getChildren().clear();
+        chartPane.getChildren().add(chart);
+
+        chartTab.setDisable(false);
+        tabPane.getSelectionModel().select(chartTab);
+        final boolean hasData = !categoryResults.isEmpty();
+        exportButton.setDisable(!hasData);
+
+        statusLabel.setText("Chart generated: " + results.size() + " categories");
+
     }
 
     @FXML
     private void onExport() {
         statusLabel.setText("Exporting... (to implement)");
     }
+
+    private void resetForNewCSV() {
+        analyzeButton.setDisable(true);
+        chartButton.setDisable(true);
+        exportButton.setDisable(true);
+
+        analysisTab.setDisable(true);
+        chartTab.setDisable(true);
+
+        results.clear();
+        records.clear();
+        chartPane.getChildren().clear();
+        analysisTable.getItems().clear();
+
+        statusLabel.setText("CSV loaded: none");
+        tabPane.getSelectionModel().select(dataTab);
+    }
+
+    private String getNameWithPercent(final CategorySummary categorySummary, final double totalAmount){
+        final double percent = categorySummary.getCount() / totalAmount * 100;
+        return categorySummary.getCategory() + " (" + String.format("%.1f", percent) + "%)";
+    }
+
 }
